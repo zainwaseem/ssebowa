caff0f4e32bdb396dcf3b8b9064733ad12937d19
Zain Waseem <zainwaseem9371@gmail.com>
2023-02-23 09:03:45 -0800

    Navbar

---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,25 @@
+import BannerBg from "../../assets/Banner.png";
+import searchicon from "../../assets/searchIcon.png";
+import "./Banner.css";
+
+const Banner = () => {
+  return (
+    <>
+      {/* <img src={BannerBg} class="img-fluid" alt="..."></img> */}
+      <div className="HeaderBannerContent">
+        <h1 className="header-title">
+          Make The World Green Again <br /> With Your Searches
+        </h1>
+        <div className="header-Search">
+          <div className="header-Search-input-icon">
+            <img src={searchicon} alt="" />
+            <input type="text" placeholder="Search the Web to Plant Trees" />
+          </div>
+          <button className="header-banner-search-button"> Search</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Banner;
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,64 @@
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import logo from "../../assets/logo.png";
+import Rectangle from "../../assets/Rectangle.png";
+const Navbar = () => {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-dark header-navbar">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">
+            <img src={logo} alt="" className="nav-logo" />
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav px-md-5  ms-md-auto mb-2 mb-lg-0 ">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  About
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Gallary
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Projects
+                </a>
+              </li>{" "}
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Team
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  <img src={Rectangle} alt="" width="23px" height="26px" />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
